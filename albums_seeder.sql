c3bcfd85fdc8d7d6dcb9ec2f67564e900538c97a
violet <vdelarosa210@gmail.com>
2017-05-31 15:12:44 -0500

    Inserted additional album information to list

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -4,4 +4,11 @@ INSERT INTO albums (artist, name, release_date, sales, genre)
 
 VALUES ('Michael Jackson', 'Thriller', '1982', '46.3', 'Pop, Rock, R&B'),
 ('ACDC', 'Back in Black', '1980', '26.1', 'Hard rock'),
-('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive rock');
+('Pink Floyd', 'The Dark Side of the Moon', '1973', '24.2', 'Progressive rock'),
+('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'R&B, Soul, Pop, Soundtrack'),
+('Meat Loaf', 'Bat Out of Hell', '1977', '21.5', 'Hard rock, Progressive rock'),
+('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock, Soft rock, Folk rock'),
+('Bee Gees', 'Saturday Night Fever', '1977', '20.6', 'Disco'),
+('Fleetwood Mac', 'Rumours', '1977', '27.9', 'Soft rock'),
+('Shania Twain', 'Come On Over', '1997', '29.6', 'Country, Pop'),
+('Alanis Morissette', 'Jagged Little Pill', '1995', '24.4', 'Alternative Rock');
