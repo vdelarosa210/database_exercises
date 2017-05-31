8947c4e176fa83732d9f746db21e4ce27569a1c4
violet <vdelarosa210@gmail.com>
2017-05-31 11:32:27 -0500

    * Corrected syntax error in 'drop if exists' to 'drop table if exists'
    * Confirmed that script is running correctly

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,12 @@
 USE codeup_test_db;
 
-DROP IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
 id INT (50),
 artist VARCHAR (255) NOT NULL,
 name VARCHAR (255) NOT NULL,
 release_date DATETIME NOT NULL,
-sales FLOAT (),
+sales FLOAT (4, 2),
 genre VARCHAR (255)
 );
