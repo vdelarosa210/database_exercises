901ea6cfb5a10fc6d848b649cfb41d2f4702a8a2
violet <vdelarosa210@gmail.com>
2017-06-01 15:53:50 -0500

    applied order of operations for  condition to select those with male gender to query

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,27 +1,36 @@
 USE employees;
 
-
 # Employees with first names 'Irena', 'Vidya', or 'Maya' - 709 rows
-SELECT first_name, last_name
+SELECT
+  first_name,
+  last_name
 FROM employees
 WHERE first_name = 'Irena'
-OR first_name = 'Vidya'
-or first_name = 'Maya';
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya';
 
-SELECT first_name, last_name
+# Add a condition to find everybody with those names who is also male — 441 rows.
+SELECT
+  first_name,
+  last_name,
+  emp_no
 FROM employees
-WHERE first_name OR ('Vidya', 'Irena', 'Maya')
-AND gender = M;
+WHERE (first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya')
+AND gender = 'M';
 
-
--- Employees whose last name starts with 'E' — 7,330 rows.
-SELECT first_name, last_name
+-- Employees whose last name starts or ends with 'E' — 7,330 rows.
+SELECT
+  first_name,
+  last_name
 FROM employees
 WHERE last_name LIKE "E%";
 
-
 -- Employees hired in the 90s — 135,214 rows.
-SELECT first_name, last_name
+SELECT
+  first_name,
+  last_name
 FROM employees
 WHERE hire_date LIKE '199%';
 -- other option is WHERE hire_date between '1990-01-01' AND '1999-12-31'
@@ -30,7 +39,6 @@ WHERE hire_date LIKE '199%';
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25';
-
 
 -- Employees with a 'q' in their last name — 1,873 rows.
 SELECT *
