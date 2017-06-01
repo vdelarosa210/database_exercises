84087aad0a4a38151ea8ec1ea41ad2d693c4730a
violet <vdelarosa210@gmail.com>
2017-06-01 16:21:51 -0500

    Created new file order_by_exercises.sql and copied content from where_exercises.sql

---
+++ order_by_exercises.sql
@@ -0,0 +1,44 @@
+USE employees;
+
+# Employees with first names 'Irena', 'Vidya', or 'Maya' - 709 rows
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya';
+
+# Add a condition to find everybody with those names who is also male — 441 rows.
+SELECT
+  first_name,
+  last_name,
+  emp_no
+FROM employees
+WHERE (first_name = 'Irena'
+      OR first_name = 'Vidya'
+      OR first_name = 'Maya')
+AND gender = 'M';
+
+-- Employees whose last name starts or ends with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE "E%E";
+
+-- Employees hired in the 90s and born on xmas — 362 rows.
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date like '%12-25'
+-- other option is WHERE hire_date between '1990-01-01' AND '1999-12-31'
+
+-- Employees born on Christmas — 842 rows.
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
