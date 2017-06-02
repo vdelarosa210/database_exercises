04bb3daae0b19fd38c0c57ca668cf15026a1ce45
violet <vdelarosa210@gmail.com>
2017-06-02 09:27:19 -0500

    Created new file and added distinct to find unique titles in the title column, also sorted by title

---
+++ group_by_exercise.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+
+# Use DISTINCT to find the unique titles in the titles table.
+SELECT DISTINCT title
+FROM employees
+GROUP BY title ASC;
+
+# Update the previous query to sort the results alphabetically.
+
+# Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY
+
+
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+
+
+# Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY.
+
